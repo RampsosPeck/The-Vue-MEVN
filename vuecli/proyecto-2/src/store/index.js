e4b9c7733888d10e126faf826a45b2293c8ce3e8
RampsosPeck <peralta.jorge.uatf@gmail.com>
2020-02-18 18:57:51 -0400

    Finish Class 5

--- vuecli/proyecto-2/src/store/index.js
+++ vuecli/proyecto-2/src/store/index.js
@@ -5,8 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+  	frutas:[
+  		{nombre: 'Manzana', cantidad: 5},
+  		{nombre: 'Pera', cantidad: 12},
+  		{nombre: 'Palta', cantidad: 15},
+  		{nombre: 'Naranja', cantidad: 7},
+  		{nombre: 'Coco', cantidad: 2}
+  	]
   },
   mutations: {
+  	aumentar(state, index){
+  		state.frutas[index].cantidad ++
+  	},
+  	reiniciar(state){
+  		state.frutas.forEach(elemento => {
+  			elemento.cantidad = 0
+  		})
+  	}
   },
   actions: {
   },
