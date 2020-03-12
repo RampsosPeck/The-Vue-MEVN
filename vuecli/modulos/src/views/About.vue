e98c20eaa03447ffb0cb52b9c57ef376de9c6dcd
RampsosPeck <peralta.jorge.uatf@gmail.com>
2020-03-12 15:29:06 -0400

    Finish lesson 18 complet Perfect

---
+++ vuecli/modulos/src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is exercices the works</h1>
+	<h2  v-for="(item, index) in tareasState" :key="index"> {{ item }} </h2>
+	<h3>{{ contador }}</h3>
+	<button @click="aumentarContador(1)"> + </button>
+	<h4>{{ titulo }}</h4>
+  </div>
+</template>
+
+<script>
+	import { mapState, mapMutations } from "vuex";
+	export default {
+		computed: {
+			...mapState('tareas', ['tareasState']),
+			...mapState(['titulo']),
+			...mapState('contador', ['contador'])
+		},
+		methods: {
+			...mapMutations('contador',['aumentarContador'])
+		}
+	};
+</script>
+
+
+
